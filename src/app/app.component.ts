be00749bdd6e57ad3e7e06581fa9352bdff082da
araujohelder <heldersilvaaraujo@gmail.com>
2018-06-17 09:40:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient) {}
+
+  inputFileChange(event) {
+
+    if (event.target.files && event.target.files[0]) {
+      const arq = event.target.files[0];
+      const formData = new FormData();
+      formData.append('arquivo', arq);
+
+      this.http.post('http://localhost:8080/arquivos', formData).subscribe( resposta => {
+        console.log('Upload realizado com sucesso!!!');
+      });
+    }
+
+  }
+
 }
